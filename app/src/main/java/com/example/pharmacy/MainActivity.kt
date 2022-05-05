8163823ff30791383d71d4fda95d1c00c7c9c7b4
Diego <diego.gamboaa03@gmail.com>
2022-05-05 08:15:46 -0400

    textView clickable para el sign In

--- app/src/main/java/com/example/pharmacy/MainActivity.kt
+++ app/src/main/java/com/example/pharmacy/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pharmacy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickTextView(view: View) {
+        Toast.makeText(applicationContext, "Remplazar por tu codigo", Toast.LENGTH_LONG)
+            .show()
     }
 }
