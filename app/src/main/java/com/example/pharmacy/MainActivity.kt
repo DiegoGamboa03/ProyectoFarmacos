2d72e3790fe4efc33c16339fa987a922d68ef966
Diego <diego.gamboaa03@gmail.com>
2022-05-06 14:07:30 -0400

    Abriendo paginas de registro desde textView

--- app/src/main/java/com/example/pharmacy/MainActivity.kt
+++ app/src/main/java/com/example/pharmacy/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.pharmacy
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.pharmacy.registerActivities.typerRegisterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +16,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickTextView(view: View) {
+        val buttonClick = findViewById<TextView>(R.id.textView_signIn)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, typerRegisterActivity::class.java)
+            startActivity(intent)
+        }
         Toast.makeText(applicationContext, "Remplazar por tu codigo", Toast.LENGTH_LONG)
             .show()
     }
+
+    fun clickLogInButton(view: View) {
+
+    }
+    /* Forma alternativa
+    fun sendMessage(view: View?) {
+        val intent = Intent(this, typerRegisterActivity::class.java)
+        startActivity(intent)
+    }*/
 }
---
+++ app/src/main/java/com/example/pharmacy/registerActivities/typerRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pharmacy.registerActivities
+
+import com.example.pharmacy.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class typerRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_typer_register)
+    }
+}
