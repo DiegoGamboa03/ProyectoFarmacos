51c07ba3c1d2e12f56a5a9c476607f9c835ae09f
Diego <diego.gamboaa03@gmail.com>
2022-05-07 14:10:52 -0400

    Pantalla registro de direccion y institucion medica a la que asiste

---
+++ app/src/main/java/com/example/pharmacy/registerActivities/PatientActivity/directionRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pharmacy.registerActivities.PatientActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class directionRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direction_register)
+    }
+}
---
+++ app/src/main/java/com/example/pharmacy/registerActivities/PatientActivity/treatmentPlaceRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pharmacy.registerActivities.PatientActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class treatmentPlaceRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_treatment_place_register)
+    }
+}
